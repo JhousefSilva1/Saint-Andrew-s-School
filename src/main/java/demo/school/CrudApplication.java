7dce0ebadb68b43fc0aed923f13711a140092781
Jose Armando Pozo Silva <jhosede23@gmail.com>
2024-02-11 19:01:59 -0400

    first commit

---
+++ src/main/java/demo/school/CrudApplication.java
@@ -0,0 +1,13 @@
+package demo.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
